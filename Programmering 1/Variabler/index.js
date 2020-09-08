efc7a4bebf3b692ac9a7672272a6071a7fcd9027
RandMohammedIT19A <70194825+RandMohammedIT19A@users.noreply.github.com>
2020-09-08 09:39:40 +0200

    räknat medelvärde

---
+++ Programmering 1/Variabler/index.js
@@ -0,0 +1,23 @@
+// skapat en variabel name och tilldela det ditt namn
+name = "Rand"
+
+
+// skriv ut "hej namn"
+
+
+console.log(`hej ${name}`)
+
+//skapa variablerna tal1, tal2,tal3
+//tilldela dessa variabler med värderna 3,4,5
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+
+
+//räkna ut medelvärdet av dem
+medelvarde = (tal1+tal2+tal3)/3
+
+// gör följande utskrift: "Medelvarde av talen 3,4,5 är "
+console.log(`(${tal1}+${tal2}+${tal3})/3 = ${medelvarde}`)
+
+let langd = prompt("ange värdet för rektangels längd")
