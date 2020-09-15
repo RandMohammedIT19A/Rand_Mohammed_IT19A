93333b2026fce8dc71810062269d7c2d53435701
RandMohammedIT19A <70194825+RandMohammedIT19A@users.noreply.github.com>
2020-09-15 09:37:09 +0200

    börjat på if-sater

---
+++ if_satser/if_satser1.js
@@ -0,0 +1,23 @@
+let tal = 25
+
+// avgöra om talet är jämnt eller udda
+
+
+// modulusoperator
+
+
+if (tal%2 === 0){
+    console.log("jämnt")
+} else {
+    console.log ("udda")
+}
+
+let tal1 = prompt("Ange Tal: ")
+
+if (tal1 > 0 ) {
+console.log("Positivt")
+} else (tal1 == 0){
+    console.log("talet är 0")
+} else {
+    console.log("negativt")
+}
