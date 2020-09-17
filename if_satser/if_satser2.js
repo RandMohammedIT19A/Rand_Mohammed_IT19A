612c72716bbf61c6486008bd9439a003b6769139
RandMohammedIT19A <70194825+RandMohammedIT19A@users.noreply.github.com>
2020-09-17 09:52:04 +0200

    insatser

---
+++ if_satser/if_satser2.js
@@ -0,0 +1,13 @@
+let age = prompt ("Hur gammal är du?")
+console.log( `Ålder: ${age}`)
+if (age >=15){
+    console.log("Du får kolla på filmen")
+} else {
+    console.log("Du får inte kolla på filmer")
+}
+
+
+
+
+
+
