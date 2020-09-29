52cc3c575069d50b06ce3a2b96affd1b6b88322f
RandMohammedIT19A <70194825+RandMohammedIT19A@users.noreply.github.com>
2020-09-29 09:44:35 +0200

    forsatser

--- Programmering 1/for_sats/for1.js
+++ Programmering 1/for_sats/for1.js
@@ -1,6 +1,8 @@
+
 let p_talserie1 = document.querySelector("#talserie1")
 let p_talserie2 = document.querySelector("#talserie2")
 let p_summa = document.querySelector("#summa")
+let p_klass = document.querySelector("#klasslista")
 let summa = 0
 
 for (let i=1; i<=10; i++){
@@ -23,3 +25,17 @@ for (let i=0; i<=10; i++){
 }
 p_summa.innerHTML += `1 + 2 + 3 +...+9+10 = ${summa}`
 console.log(summa)
+
+//skapa en lista med elever
+
+let elever = ["Rand", "sdasdasdasd" ] 
+
+
+p_klass.innerHTML = "Klass IT19A <br/>"
+
+for (let k = 0; k < elever.length; k++) {
+    p_klass.innerHTML += `${k+1}. ${elever[k]} <br/>`
+p_klass.innerHTML += elever[0]
+
+
+}
