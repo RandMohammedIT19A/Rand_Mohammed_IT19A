c5c9de7019f25a9dc0ed97d821682b6f794d15aa
RandMohammedIT19A <70194825+RandMohammedIT19A@users.noreply.github.com>
2020-10-13 09:38:58 +0200

    funktioner

---
+++ Programmering 1/funktioner/funktioner1.js
@@ -0,0 +1,23 @@
+let namn = "Rand" 
+
+function sayHello(){
+    let alder = 17
+    console.log(`Hej ${namn}, du är ${alder} år gammal`)
+}
+sayHello()
+sayHello()
+
+function addera(tal1, tal2){
+    let summa = tal1+tal2
+    return summa
+}
+
+console.log(`2+5=${addera(2,5)}`)
+
+function dividera(tal1, tal2){
+    let kvot = tal1/tal2
+    return kvot
+}
+
+console.log(`35/7 = ${dividera(35,7)}`)
+console.log(`2/0 = ${dividera(2,0)}`)
